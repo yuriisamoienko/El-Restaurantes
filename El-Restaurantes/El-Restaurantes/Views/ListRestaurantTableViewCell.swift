662a18d9915ad6b058ef95a43bfef71da3e86de6
Yurii Samoienko <yurii.samoienko@gmail.com>
2021-08-01 23:15:55 +0300

    refactored ListRestaurantTableViewCell

--- El-Restaurantes/El-Restaurantes/Views/ListRestaurantTableViewCell.swift
+++ El-Restaurantes/El-Restaurantes/Views/ListRestaurantTableViewCell.swift
@@ -8,7 +8,13 @@
 import UIKit
 import MapKit
 
+/*
+ Cell for ListRestaurantsVC
+ */
+
 class ListRestaurantTableViewCell: UITableViewCellBase {
+    
+    // MARK: Public Properties
     
     public var name: String = "" {
         didSet {
@@ -33,36 +39,27 @@ class ListRestaurantTableViewCell: UITableViewCellBase {
     
     override open var contentView: UIView {
         get {
+            // it called first time when cell kind of "loaded from nib".
             configureLayout()
-//            if horizontalPaddingsStackView.superview == nil {
-//                self.addSubview(horizontalPaddingsStackView)
-//                horizontalPaddingsStackView.fillParentFrame()
-//                [
-//                    horizontalPaddingsStackView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-//                    horizontalPaddingsStackView.topAnchor.constraint(equalTo: self.topAnchor),
-//                    horizontalPaddingsStackView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-//                    horizontalPaddingsStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-//                ].activate()
-//            }
             return super.contentView
         }
     }
     
-    private var isLayoutConfigured = false
+    // MARK: Private Properties
     
+    // UI
     private let nameLabel = UILabel()
     private let priceLabel = UILabel()
     private let addressLabel = UILabel()
     private let distanceLabel = UILabel()
     private lazy var distanceLabelWidthConstraint = distanceLabel.setWidthConstraint(constant: 0)
-//    private let horizontalPaddingsStackView = UIStackView(axis: .horizontal)
 
-    override func postInit() {
-        super.postInit()
-        // Initialization code
-    }
+    private var isLayoutConfigured = false
+
     
-    func configureLayout() {
+    // MARK: Private Functions
+    
+    private func configureLayout() {
         if isLayoutConfigured == true {
             return
         }
@@ -73,27 +70,22 @@ class ListRestaurantTableViewCell: UITableViewCellBase {
         nameLabel.font = .systemFont(ofSize: 20)
         nameLabel.numberOfLines = 0
         nameLabel.textAlignment = .left
-//        nameLabel.setHeightConstraint(constant: 16, relatedBy: .greaterThanOrEqual)
-//        nameLabel.backgroundColor = .yellow
-//        nameLabel.setWidthConstraint(constant: 0, relatedBy: .greaterThanOrEqual)
         
         priceLabel.font = .boldSystemFont(ofSize: 18)
         priceLabel.textAlignment = .right
-//        priceLabel.backgroundColor = .green
-//        priceLabel.numberOfLines = 0
         priceLabel.setWidthConstraint(constant: 50)
         priceLabel.textColor = .systemGreen
         
         let firstRowStackView = UIStackView(axis: .horizontal)
-//        firstRowStackView.distribution = .fillProportionally
-        
-        firstRowStackView.addArrangedSubviews(nameLabel, priceLabel)
+        firstRowStackView.addArrangedSubviews(
+            nameLabel,
+            priceLabel
+        )
         firstRowStackView.setHeightConstraint(constant: 20, relatedBy: .greaterThanOrEqual)
         
         addressLabel.font = .systemFont(ofSize: 14)
         addressLabel.numberOfLines = 0
         addressLabel.textColor = .systemGray
-//        addressLabel.textAlignment = .left
         addressLabel.setHeightConstraint(constant: 15, relatedBy: .greaterThanOrEqual)
         
         distanceLabel.textAlignment = .right
@@ -102,41 +94,16 @@ class ListRestaurantTableViewCell: UITableViewCellBase {
         let secondRowStackView = UIStackView(axis: .horizontal)
         secondRowStackView.addArrangedSubviews(addressLabel, distanceLabel)
         secondRowStackView.spacing = 5
-//                secondRowStackView.distribution = .equalSpacing
         
-//        let paddingTop = UIView()
-//        let paddingBottom = UIView()
-//        [paddingTop, paddingBottom].forEach {
-//            $0.setHeightConstraint(constant: 15)
-//        }
         let contentStackView = UIStackView(axis: .vertical)
         contentStackView.spacing = 5
         contentStackView.addArrangedSubviews(
             firstRowStackView,
             secondRowStackView
         )
-        
-//        let paddingLeft = UIView()
-//        let paddingRight = UIView()
-//        [paddingLeft, paddingRight].forEach {
-//            $0.setWidthConstraint(constant: 20)
-//        }
-//        horizontalPaddingsStackView.addArrangedSubviews(
-//            paddingLeft,
-//            contentStackView,
-//            paddingRight
-//        )
-        
+
         self.addSubview(contentStackView)
-//        horizontalPaddingsStackView.fillParentFrame()
-//        horizontalPaddingsStackView.fillParentFrameByConstraints()
         contentStackView.pinEdges(to: self.layoutMarginsGuide, spacingHorizontal: 10, spacingVertical: 5)
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
     // MARK: Private Functions
@@ -154,11 +121,13 @@ class ListRestaurantTableViewCell: UITableViewCellBase {
     }
     
     private func onDistanceDidSet() {
+        // need format distance in readable format
         let df = MKDistanceFormatter()
         df.unitStyle = .abbreviated
         df.units = .metric
         let prettyString = df.string(fromDistance: distance)
         distanceLabel.text = prettyString
+        // calc width constraint to fit text
         distanceLabelWidthConstraint.constant = distanceLabel.textPixelWidth()
     }
 }
